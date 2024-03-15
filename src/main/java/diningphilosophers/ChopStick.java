719e0917b29f2a41338a69d71e252c9d1c394bc3
J-b-benetti <j.b.benetti81@gmail.com>
2024-03-15 15:37:28 +0100

    Commit of final code

--- src/main/java/diningphilosophers/ChopStick.java
+++ src/main/java/diningphilosophers/ChopStick.java
@@ -1,7 +1,12 @@
 package diningphilosophers;
 
-public class ChopStick {
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class ChopStick {
+    private final Lock verrou = new ReentrantLock();
+    private final Condition condVerrou = verrou.newCondition();
     private static int stickCount = 0;
     private boolean iAmFree = true;
     private final int myNumber;
@@ -10,7 +15,7 @@ public class ChopStick {
         myNumber = ++stickCount;
     }
 
-    synchronized public boolean tryTake(int delay) throws InterruptedException {
+    /*synchronized public boolean tryTake(int delay) throws InterruptedException {
         if (!iAmFree) {
             wait(delay);
             if (!iAmFree) // Toujours pas libre, on abandonne
@@ -21,16 +26,37 @@ public class ChopStick {
         iAmFree = false;
         // Pas utile de faire notifyAll ici, personne n'attend qu'elle soit occupée
         return true; // Succès
+    }*/
+
+    public boolean tryTake(int timeout) throws InterruptedException {
+        verrou.lock();
+        try {
+            while (!iAmFree) {
+                condVerrou.await();
+                return false; // Echec
+            }
+            iAmFree = false;
+
+            return true;
+
+        } finally {
+            verrou.unlock();
+        }
     }
 
-    synchronized public void release() {
-        iAmFree = true;
-        notifyAll();
-        System.out.println("Stick " + myNumber + " Released");
+    public void release() {
+        verrou.lock();
+        try {
+            iAmFree = true;
+            condVerrou.signalAll();
+            System.out.println("Stick " + myNumber + " Released");
+        } finally {
+            verrou.unlock();
+        }
     }
 
     @Override
     public String toString() {
-        return "Stick#" + myNumber;
+        return "Stick#" + this.myNumber;
     }
 }
